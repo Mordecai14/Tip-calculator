d684f00f6a839c4ea61499f1c22522d58f57a534
Aramen Meza <arammendoza.97@gmail.com>
2021-04-28 12:30:28 -0500

    Init Project

---
+++ Tipcalculator/Tipcalculator/Tipcalculator/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Tipcalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            var total = (decimal.Parse(txtTotal.Text));
+            var propina = (decimal.Parse(txtPropina.Text));
+            var personas = (decimal.Parse(txtNoPersonas.Text));
+
+            var totalPropina = ((total * propina) / 100);
+
+            labelPropina.Detail = totalPropina.ToString("C");
+            labelTotal.Detail = (totalPropina + total).ToString("C");
+            labelPropinaPorPersona.Detail = (totalPropina / personas).ToString("C");
+            labelTotalPorpersona.Detail = ((total + totalPropina) / personas).ToString("C");
+        }
+
+    }
+}
